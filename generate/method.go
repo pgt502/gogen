14eaab2c6f508ca5c22ac2c9e88f144c3ff1e5e8
pawel.terpilowski <pawel.terpilowski@guardtime.com>
2018-10-19 23:12:04 +0100

    added tests and fixed support for array params

--- generate/method.go
+++ generate/method.go
@@ -1,6 +1,7 @@
 package generate
 
 import (
+	"fmt"
 	"go/types"
 	"path"
 	"strings"
@@ -22,65 +23,57 @@ func (m Method) Name() string {
 	return m.fn.Name()
 }
 
+// ParamTypesVariadic returns the list of types for the params with ... for variadic params
+func (m Method) ParamTypesVariadic() []string {
+	sig := m.Signature()
+	types := m.listTypeNames(sig.Params())
+	n := len(types)
+	if n > 0 && sig.Variadic() {
+		types[n-1] = strings.Replace(types[n-1], "[]", "...", 1)
+	}
+	return types
+}
+
 // ParamTypes returns the list of types for the params
 func (m Method) ParamTypes() []string {
 	sig := m.Signature()
-	types := m.listTypes(sig.Params())
-	n := len(types)
-	if n > 0 && sig.Variadic() {
-		types[n-1] = strings.Replace(types[n-1], "[]", "...", 1)
-	}
-	return types
-}
-
-// ParamTypes returns the list of types for the params
-func (m Method) ParamTypesClean() []string {
-	sig := m.Signature()
 	types := m.listTypeNames(sig.Params())
-	n := len(types)
-	if n > 0 && sig.Variadic() {
-		types[n-1] = strings.Replace(types[n-1], "[]", "...", 1)
-	}
-	return types
-}
-
-func (m Method) ParamTypesCleanArr() []string {
-	sig := m.Signature()
-	types := m.listTypeNames(sig.Params())
-
 	return types
 }
 
 // ReturnTypes returns the list of types for the params
 func (m Method) ReturnTypes() []string {
 	sig := m.Signature()
-	return m.listTypes(sig.Results())
-}
-
-// ReturnTypes returns the list of types for the params
-func (m Method) ReturnTypesClean() []string {
-	sig := m.Signature()
 	return m.listTypeNames(sig.Results())
 }
 
-func (m Method) listTypes(t *types.Tuple) []string {
-	num := t.Len()
-	list := make([]string, num)
-	for i := 0; i < num; i++ {
-		//list[i] = types.TypeString(t.At(i).Type(), m.gen.qf)
-		list[i] = types.TypeString(t.At(i).Type(), nil)
+// ReturnTypesVariadic returns the list of types for the params with ... for variadic params
+func (m Method) ReturnTypesVariadic() []string {
+	sig := m.Signature()
+	types := m.listTypeNames(sig.Results())
+	n := len(types)
+	if n > 0 && sig.Variadic() {
+		types[n-1] = strings.Replace(types[n-1], "[]", "...", 1)
 	}
-	return list
+	return types
 }
 
 func (m Method) listTypeNames(t *types.Tuple) []string {
 	num := t.Len()
 	list := make([]string, num)
 	for i := 0; i < num; i++ {
-		//list[i] = types.TypeString(t.At(i).Type(), m.gen.qf)
-		//list[i] = types.TypeString(t.At(i).Type(), nil)
 		p := t.At(i).Type().String()
-		list[i] = path.Base(p)
+		if strings.Contains(p, "/") {
+			if strings.HasPrefix(p, "[]*") {
+				list[i] = fmt.Sprintf("[]*%s", path.Base(p))
+			} else if strings.HasPrefix(p, "[]") {
+				list[i] = fmt.Sprintf("[]%s", path.Base(p))
+			} else {
+				list[i] = path.Base(p)
+			}
+		} else {
+			list[i] = p
+		}
 	}
 	return list
 }
--- testdata/order.go
+++ testdata/order.go
@@ -1,9 +1,24 @@
 package testdata
 
+import "time"
+
 type Order struct {
 	Product   string  `db:"name"`
 	Id        string  `json:"id" db:"id,pk"`
 	Price     float64 `db:"price"`
 	CreatedAt int64   `db:"created_at"`
 	CreatedBy string  `db:"created_by"`
+}
+
+type TestType struct {
+	Name      string
+	Id        int64     `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+type TestInterface interface {
+	Method1() error
+	Method2(string, interface{}) (interface{}, error)
+	Method3([]*TestType) []TestType
+	Method4(...string)
 }
